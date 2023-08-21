7d6f01a71937f35816e010f42a6e99a019d4ed97
Tim Reynolds <tmrdev@gmail.com>
2023-08-21 18:11:36 -0400

    new hash for signed version of app

--- Casks/sparkplate.rb
+++ Casks/sparkplate.rb
@@ -1,6 +1,6 @@
 cask "sparkplate" do
   version "1.0.0"
-  sha256 "25d54d9202e23e88bfb109f964078da2be7b998b7d08ad6a4ee349b734c18d81"
+  sha256 "f9bff9256c4c95bf1a53b9c074e6a3f44d35e75043a404f306c1bd09121e93e7"
 
   url "https://github.com/GreenfireInc/homebrew-sparkplate/releases/download/v#{version}/Sparkplate.zip"
   name "sparkplate"
