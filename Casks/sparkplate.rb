dfc946722544da7537f803d546de1eb43120d3b1
Tim Reynolds <tmrdev@gmail.com>
2023-08-30 17:30:54 -0400

    adding zap trash uninstall support to Cask

--- Casks/sparkplate.rb
+++ Casks/sparkplate.rb
@@ -8,4 +8,15 @@ cask "sparkplate" do
   homepage "https://github.com/GreenfireInc/Sparkplate.Vue"
 
   app "Sparkplate.app"
+
+  zap trash: [
+  "~/Library/Application Support/sparkplate",
+  "~/Library/Preferences/com.sparkplate.app.plist",
+  "~/Library/Preferences/io.greenfire.sparkplate-.plist",
+  "~/Library/Preferences/io.greenfire.sparkplate.helper.plist",
+  "~/Library/Preferences/io.greenfire.sparkplate.plist",
+  "~/Library/Saved Application State/com.sparkplate.app.savedState",
+  "~/Library/Saved Application State/io.greenfire.sparkplate.savedState",
+]
+
 end
